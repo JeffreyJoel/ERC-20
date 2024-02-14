5753880bd9715073773076d5cec80da65120e08f
JeffreyJoel <jowoloko@gmail.com>
2024-02-14 18:16:06 +0100

    add token parameters

---
+++ contracts/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Token {
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+    }
+}
